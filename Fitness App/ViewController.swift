3df116488b9c1e2650d540d0f73a346b2f73497b
Daniel Dodds (s5119004) <s5119004@mac38580.mac.bournemouth.ac.uk>
2019-04-01 12:44:15 +0100

    Delagated the Location Manager

--- Fitness App/ViewController.swift
+++ Fitness App/ViewController.swift
@@ -9,10 +9,19 @@ class ViewController: UIViewController {
         
         let locationManager=CLLocationManager()
         
+        
+        locationManager.requestAlwaysAuthorization()
+        locationManager.delegate = self
+        locationManager.startUpdatingLocation()
 
     }
 
 
 }
 
+extension ViewController: CLLocationManagerDelegate{
+    func locationManager(_ manager: CLLocationManager, didUpdateLocations locations: [CLLocation]) {
+        print(locations.last!)
+    }
+}
 
