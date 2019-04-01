0082d4ff9eda5d7776825cfe63d2944b2a13421c
Olivia Irwin (s5113143) <s5113143@mac38582.mac.bournemouth.ac.uk>
2019-04-01 12:59:57 +0100

    coordinates

--- Fitness App/ViewController.swift
+++ Fitness App/ViewController.swift
@@ -15,6 +15,17 @@ class ViewController: UIViewController {
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
 
+        let  = CLLocationCoordinate2D(latitude: 51.490929, longitude: -0.127715)
+        let  = CLLocationCoordinate2D(latitude: 51.508804, longitude: -0.127544)
+        let  = CLLocationCoordinate2D(latitude: 51.507487, longitude: -0.099561)
+        let  = CLLocationCoordinate2D(latitude: 51.49069, longitude: -0.158916)
+        let  = CLLocationCoordinate2D(latitude: 51.496696, longitude: -0.17243)
+        
+        let TateBritainMuseumAnnotation = CustomAnnotation(coordinate: TateBritainMuseumCoordinate, title: "")
+        let TheNationalGalleryAnnotation = CustomAnnotation(coordinate: TheNationalGalleryCoordinate, title: "")
+        let TateModernMuseumAnnotation = CustomAnnotation(coordinate: TateModernMuseumCoordinate, title: "")
+        let SaatchiGalleryAnnotation = CustomAnnotation(coordinate: SaatchiGalleryCoordinate, title: "")
+        let VictoriaAndAlbertMuseumAnnotation = CustomAnnotation(coordinate: VictoriaAndAlbertMuseumCoordinate, title: "")
         
         
         
