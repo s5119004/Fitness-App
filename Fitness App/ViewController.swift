245c19e1c199ce1f2b7d9a7660e24e7d50a4bcea
Miles Darby (s5116926) <s5116926@mac42944.student.bournemouth.ac.uk>
2019-04-04 13:17:07 +0100

    changing location

--- Fitness App/ViewController.swift
+++ Fitness App/ViewController.swift
@@ -19,21 +19,9 @@ class ViewController: UIViewController {
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
 
-        
-        let locationOneCoordinate = CLLocationCoordinate2D(latitude: 50.713792, longitude: -1.874537)
-        let locationTwoCoordinate = CLLocationCoordinate2D(latitude: 50.718349, longitude: -1.842870)
-        let locationThreeCoordinate = CLLocationCoordinate2D(latitude: 50.717936, longitude: -1.866509)
-        let locationFourCoordinate = CLLocationCoordinate2D(latitude: 50.718710, longitude: -1.877140)
-        let locationFiveCoordinate = CLLocationCoordinate2D(latitude: 50.723903, longitude: -1.888981)
-        
-        let locationOneAnnotation = CustomAnnotation(coordinate: locationOneCoordinate, title: "locationOne")
-        let locationTwoAnnotation = CustomAnnotation(coordinate: locationTwoCoordinate, title: "locationTwo")
-        let locationThreeAnnotation = CustomAnnotation(coordinate: locationThreeCoordinate, title: "locationThree")
-        let locationFourAnnotation = CustomAnnotation(coordinate: locationFourCoordinate, title: "locationFour")
-        let locationFiveAnnotation = CustomAnnotation(coordinate: locationFiveCoordinate, title: "locationFive")
 
         
-        MapView.addAnnotations([locationOneAnnotation, locationTwoAnnotation, locationThreeAnnotation,locationFourAnnotation,locationFiveAnnotation])
+        
         
     }
 
