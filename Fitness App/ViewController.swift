69c5ba2ce8013a1fb79f61d9fba0ac4087a7db20
Miles Darby (s5116926) <s5116926@mac42944.student.bournemouth.ac.uk>
2019-04-04 13:36:07 +0100

    part 1 of the new location system

--- Fitness App/ViewController.swift
+++ Fitness App/ViewController.swift
@@ -16,7 +16,27 @@ class MapViewController: UIViewController {
         locationManager.requestAlwaysAuthorization()
         locationManager.delegate = self
         locationManager.startUpdatingLocation()
-
+        
+    }
+    
+    func generatePoints(from location: CLLocation)
+        
+    let coordinates = getRandomCoordinates(from: location, itemCount: 3)
+    
+    for coordinates in coordinates {
+    let title = "Distance: \(getDistance(from: location, to: coordinate))k"
+        let annotationOne = CustomAnnotation(coordinate: coordinate, title: title)
+        MapView.addAnnotation(annotationOne)
+        MapView.showAnnotations(MapView.annotations, animated: false)
+    }
+ }
+    func getDistance(from: CLLocation, to: CLLocationCoordinate2D) -> CLLocationDistance {
+     let coordinateLocation = CLLocation(latitude: to.latitude, longitude: to.longitude)
+     return from.distance(from: coordinateLocation)/1000
+    }
+    
+    
+    
 
         
         
