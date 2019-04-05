f6770a17cd7367371a76495c9cbb2bf08a51415d
Daniel Dodds (s5119004) <s5119004@mac42938.student.bournemouth.ac.uk>
2019-04-05 10:33:43 +0100

    Image Picker
    Profile Pic is uploading

--- Fitness App/SecondViewController.swift
+++ Fitness App/SecondViewController.swift
@@ -2,14 +2,15 @@ import UIKit
 
 class SecondViewController: UIViewController, UINavigationControllerDelegate, UIImagePickerControllerDelegate{
     
+    @IBOutlet weak var profileImage: UIImageView!
+
+    
     override func viewDidLoad() {
         super.viewDidLoad()
     }
     
-    @IBOutlet weak var UIImageView: UIImageView!
     
-    
-    @IBAction func button(_ sender: UIButton) {
+    @IBAction func getImageButton(_ sender: UIButton) {
         let image = UIImagePickerController()
         image.delegate = self
         
@@ -18,24 +19,26 @@ class SecondViewController: UIViewController, UINavigationControllerDelegate, UI
             self.present(image, animated: true, completion: nil)
         }else{
             print("camera not available")
+        }
+        
+        image.sourceType = UIImagePickerController.SourceType.photoLibrary
+        image.allowsEditing = false
+        self.present(image, animated: true) {
     }
     
-    //@IBAction func startButton(_ sender: UIButton) {
     }
+//    @IBAction func startButton(_ sender: UIButton) {
+//
+//    }
     
-    //imagesourceType = UIImagePickerController.SourceType.photoLibrary
-    //image.allowsEditing = false
-    //self.present(image, animated: true) {
-    }
-    
-//}
 
-//func imagePickerContoller(_picker:UIImagePickerController, didFinishPickingMediaWithInfo info:
-    //[UIImagePickerController.InfoKey : Any]) {
-    //if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
-      //  ImageControl.image = image
-   // }else{
-        //error
-   // }
-    //self.dismiss(animated: true, completion: nil)
-//}
+    func imagePickerController(_ picker:UIImagePickerController, didFinishPickingMediaWithInfo info:
+    [UIImagePickerController.InfoKey : Any]) {
+    if let image = info[UIImagePickerController.InfoKey.originalImage] as? UIImage {
+       profileImage.image = image
+    }else{
+       print("error")
+    }
+    self.dismiss(animated: true, completion: nil)
+}
+}
