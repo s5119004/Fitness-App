203109d214faa120581fb9448ac94f32e2f0e798
Monica Cruz (s5107958) <s5107958@mac42944.mac.bournemouth.ac.uk>
2019-05-03 09:07:21 +0100

    Deleting comment

--- Fitness App/ThirdViewController.swift
+++ Fitness App/ThirdViewController.swift
@@ -1,9 +1,1 @@
-//
-//  ThirdViewController.swift
-//  Fitness App
-//
-//  Created by monica  on 26/04/2019.
-//  Copyright © 2019 Daniel Dodds (s5119004). All rights reserved.
-//
-
 import Foundation
